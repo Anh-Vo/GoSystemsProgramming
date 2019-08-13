25a87e04c104eb049e0da742f691a11a06c00b2d
Anh Vo <AnhVoo.Dev@gmail.com>
2019-08-13 15:28:01 -0700

    Adding code to print min and max when program exits

--- exercises/part1/stopAt0.go
+++ exercises/part1/stopAt0.go
@@ -2,11 +2,14 @@ package main
 
 import (
 	"fmt"
+	"math"
 	"strconv"
 )
 
 func main() {
 
+	min := math.MaxInt32
+	max := math.MinInt32
 	for {
 		fmt.Println("Input a number to continue. 0 to quit.")
 		var answer string
@@ -15,8 +18,20 @@ func main() {
 		if err == nil {
 			if input == 0 {
 				fmt.Println("Got 0, bye now.")
+				fmt.Printf("Min: %d, Max: %d", min, max)
 				break
 			} else {
+				if input >= max {
+					max = input
+					fmt.Println("new max")
+					fmt.Println(max)
+				}
+
+				if input <= min {
+					min = input
+					fmt.Println("new min")
+				}
+
 				fmt.Printf("Got %d, here we go again \n", input)
 			}
 		} else {
