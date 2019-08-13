cfa5ce02c072dbb6866dbee827b1dcae2973acc8
Anh Vo <anhvoo.dev@gmail.com>
2019-08-13 16:34:26 -0700

    Another verion of the stopAt0. This time taking in command line arguments instead of looking for user interaction. This form is much better because we then take the human out of the loop and pass it arguments using a pipe.

---
+++ exercises/part1/stopAt0withArgs.go
@@ -0,0 +1,34 @@
+package main
+
+import (
+	"fmt"
+	"math"
+	"os"
+	"strconv"
+)
+
+func main() {
+	arguments := os.Args
+	min := math.MaxInt32
+	max := math.MinInt32
+
+	for i := 0; i < len(arguments); i++ {
+		num, err := strconv.Atoi(arguments[i])
+		if err == nil {
+			if num == 0 {
+				fmt.Println("Found a 0. Stopping.")
+				break
+			} else {
+				if num > max {
+					max = num
+				}
+				if num < min {
+					min = num
+				}
+			}
+		}
+	}
+	if len(arguments) > 1 {
+		fmt.Printf("Min: %d, Max: %d", min, max)
+	}
+}
